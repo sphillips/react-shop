3e77d540f2a13591f5d80fd97436ed2b465e0338
Sean Phillips <seanphillips@protonmail.com>
2019-12-17 11:43:43 -0500

    initial commit - from create react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Homepage from './homepage.component'
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div>
+        <Homepage />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
