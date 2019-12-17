5d172e18694361e3dfe3a0ffb873a8ba6d77e916
Sean Phillips <seanphillips@protonmail.com>
2019-12-17 12:45:50 -0500

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Route } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/homepage/homepage.component'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Homepage />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <Route exact path="/" component={Homepage} />
+    </div>
+  )
 }
 
 export default App
