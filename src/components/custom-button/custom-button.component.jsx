0f0b45931fb60e9df958c5f5b99428256401c921
Sean Phillips <seanphillips@protonmail.com>
2019-12-19 15:58:46 -0500

    updates quantity on cart items when added

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import './custom-button.styles.scss'
 
-function CustomButton({ children, isGoogleSignIn, ...otherProps }) {
+function CustomButton({ children, isGoogleSignIn, inverted, ...otherProps }) {
   return (
-    <div className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}{...otherProps}>
+    <div className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}{...otherProps}>
       {children}
     </div>
   )
